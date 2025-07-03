75145d3da17f5c9237bd6a17208da6e04e8e2ab2
sanjay vishwakrma <68105994+SanjayVishwakrma@users.noreply.github.com>
2025-07-03 13:06:58 +0530

    Update payment.js
    aaa

--- payment.js
+++ payment.js
@@ -3,6 +3,8 @@ const africastalking = require('./africastalking');
 async function initiatePayment(phoneNumber, amount) {
   const payments = africastalking.PAYMENTS;
 
+  console.log("✅ Payment --------------:", payments);
+
   const options = {
     productName: "MyProduct", // 🔁 Replace with your dashboard product
     phoneNumber: phoneNumber,
@@ -12,10 +14,11 @@ async function initiatePayment(phoneNumber, amount) {
       reason: "Lottery Ticket"
     }
   };
+  console.log("✅ options --------------:", options);
 
   try {
     const result = await payments.mobileCheckout(options);
-    console.log("✅ Payment Initiated:", result);
+    console.log("✅ Payment Initiated:---------", result);
     return { success: true, transactionId: result.transactionId };
   } catch (err) {
     console.error("❌ Payment Failed:", err);
