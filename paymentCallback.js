9e9b90483932ae36a7924208c55ff9d9956ecc1b
sanjay vishwakrma <68105994+SanjayVishwakrma@users.noreply.github.com>
2025-07-03 18:09:05 +0530

    Update paymentCallback.js
    sss

--- paymentCallback.js
+++ paymentCallback.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 router.post('/payment-callback', (req, res) => {
@@ -19,11 +19,62 @@ router.post('/payment-callback', (req, res) => {
   res.sendStatus(200);
 });
 
-router.post('/valid', (req, res) => {
+
+// ✅ /valid route with required response
+router.get('/valid', (req, res) => {
   // You can add validation checks here if needed
   res.status(200).json({
     status: "Validated"
   });
+});
+
+
+// module.exports = router;
+
+
+
+// 🔐 Authentication credentials
+const credentials = {
+  apiKey: "Te7zXfg0f",
+  username: "sandbox" // Use 'sandbox' for testing
+};
+
+// Require the AT package
+const AfricasTalking = require("africastalking")(credentials);
+
+const airtime = AfricasTalking.AIRTIME;
+// Send Airtime route
+router.post("/", (req, res) => {
+  const { options = {
+            maxNumRetry: 3, // Will retry the transaction every 60seconds for the next 3 hours.
+            recipients: [{
+                phoneNumber: "+27665773560",
+                currencyCode: "KES",
+                amount: "5"
+            }] 
+          }
+        } = req.body || res.status(400).json({error: "Both 'maxNumRetry' and 'recipients details' are required"});
+
+  airtime
+    .send(options)
+    .then(response => {
+      console.log(response);
+      res.json(response);
+    })
+    .catch(error => {
+      console.log(error);
+      res.json(error.toString());
+    });
+});
+
+// Create callback route
+router.post("/status", async( req, res ) => {
+  console.log(req.body);
+
+  res.status(200).json({
+      status: "success",
+      message: "Airtime received successfully"
+  })
 });
 
 module.exports = router;
